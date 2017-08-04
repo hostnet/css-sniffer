5c0c2b23b09f28da4d82117146555a0a2a29a43c
Yannick de Lange <ydelange@hostnet.nl>
2017-08-04 15:52:03 +0200

    Added Quotes sniff

---
+++ src/Sniff/QuoteTypeSniff.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright 2015-2017 Hostnet B.V.
+ */
+
+namespace Hostnet\Component\CssSniff\Sniff;
+
+use Hostnet\Component\CssSniff\File;
+use Hostnet\Component\CssSniff\SniffInterface;
+use Yannickl88\Component\CSS\Token;
+
+final class QuoteTypeSniff implements SniffInterface
+{
+    private $quote;
+
+    public function __construct(string $quote = '"')
+    {
+        $this->quote = $quote;
+    }
+
+    public function register(): array
+    {
+        return [
+            Token::T_STRING,
+        ];
+    }
+
+    public function process(File $file, int $stack_ptr): void
+    {
+        $token = $file->get($stack_ptr);
+
+        if ($token->chars[0] !== $this->quote || $token->chars[strlen($token->chars) - 1] !== $this->quote) {
+            $file->addViolation(
+                sprintf('Text should use %s as quotes.', $this->quote),
+                $token->lines[0],
+                $token->offsets[0],
+                $token->offsets[0] + strlen($token->chars)
+            );
+        }
+    }
+}
---
+++ test/Sniff/QuoteTypeSniffTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright 2017 Hostnet B.V.
+ */
+declare(strict_types=1);
+namespace Hostnet\Component\CssSniff\Sniff;
+
+use Hostnet\Component\CssSniff\File;
+use Hostnet\Component\CssSniff\Sniffer;
+use Hostnet\Component\CssSniff\Violation;
+use PHPUnit\Framework\TestCase;
+use Yannickl88\Component\CSS\Tokenizer;
+
+/**
+ * @covers \Hostnet\Component\CssSniff\Sniff\QuoteTypeSniff
+ */
+class QuoteTypeSniffTest extends TestCase
+{
+    /**
+     * @var Sniffer
+     */
+    private $sniffer;
+
+    protected function setUp()
+    {
+        $this->sniffer = new Sniffer();
+        $this->sniffer->addSniff(new QuoteTypeSniff());
+    }
+
+    public function testSniff()
+    {
+        $file = new File((new Tokenizer())->tokenize(file_get_contents(__DIR__ . '/fixtures/quotes.less')));
+
+        $this->sniffer->process($file);
+
+        self::assertEquals([
+            new Violation('Text should use " as quotes.', 5, 27, 37),
+        ], $file->getViolations());
+    }
+}
