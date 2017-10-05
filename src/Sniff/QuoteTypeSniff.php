b2a78d8854d5ec00cb1d587dcdd85d32fbd2c6cf
Yannick de Lange <ydelange@hostnet.nl>
2017-10-05 15:58:22 +0200

    Ignore backticks in the sniff

--- src/Sniff/QuoteTypeSniff.php
+++ src/Sniff/QuoteTypeSniff.php
@@ -30,6 +30,11 @@ final class QuoteTypeSniff implements SniffInterface
     {
         $token = $file->get($stack_ptr);
 
+        // Ignore backticks
+        if ($token->chars[0] === '`') {
+            return;
+        }
+
         if ($token->chars[0] !== $this->quote || $token->chars[strlen($token->chars) - 1] !== $this->quote) {
             $file->addViolation(
                 sprintf('Text should use %s as quotes.', $this->quote),
